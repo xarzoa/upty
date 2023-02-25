7b5c5bbb520c26a9ca4e98423fa0a62ecc61f24f
X <xarzoa@gmail.com>
2023-02-25 16:26:52 +0530

    finally

--- app/src/pages/api/getHost.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ headers:  });
-  } else {
-    res.status(502).json({
-      message: 'This route only accepts GET reqs!',
-    });
-  }
-}
