4cfaf7d2488e4128293ec9193ccbda0075355455
X <xarzoa@gmail.com>
2023-02-25 15:08:01 +0530

    typo

--- app/src/pages/api/getHost.js
+++ app/src/pages/api/getHost.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    res.status(200).json({ req.headers });
+    res.status(200).json({ headers: req.headers });
   } else {
     res.status(502).json({
       message: 'This route only accepts GET reqs!',
